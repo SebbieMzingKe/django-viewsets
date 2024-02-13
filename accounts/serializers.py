f4387e7c1b845fe5f9e6347c62c12d3e52a5bbd1
Sebbie <sebbievilar2@gmail.com>
2024-02-13 16:31:25 +0300

    authenitation

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -20,3 +20,10 @@ class SignUPSerializer(serializers.ModelSerializer):
                     if email_exists:
                         raise validationError("email has already been used")
                     return super().validate(attrs)
+                
+                def create(self, validated_data):
+                    password = validated_data.pop(password)
+                    user = super().create(validated_data)
+                    user.set_password(password)
+                    user.save()
+                    return user
--- blog/settings.py
+++ blog/settings.py
@@ -56,6 +56,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'blog.urls'
 
+REST_FRAMEWORK = {
+    "NON_FIELD_ERRORS_KEY":"errors",
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
